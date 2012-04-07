35fa07c1c4c324f27814cc2140305039167b2a50
Jacob Atzen <jacob@incremental.dk>
2012-04-07 17:54:13 +0200

    Rework the install script

--- install.sh
+++ install.sh
@@ -1,50 +1,134 @@
 #!/bin/bash
 
+######################################################################
+#
 # This runs as root on the server
+#
+######################################################################
 
-chef_binary=/var/lib/gems/1.9.1/bin/chef-solo
-serveradminpassword=asdf
+rubyversion=1.9.3-p125
+rubydir=/usr/local
+rubybindir=$rubydir/bin
+gembinary=$rubybindir/gem
+rubybinary=$rubybindir/ruby
+chefbinary=$rubybindir/chef-solo
+knifebinary=$rubybindir/knife
 
-# Are we on a vanilla system?
-if ! test -f "$chef_binary"
-then
-    export DEBIAN_FRONTEND=noninteractive
-    # Upgrade headlessly (this is only safe-ish on vanilla systems)
-    aptitude update &&
-    apt-get -o Dpkg::Options::="--force-confnew" \
-        --force-yes -fuy dist-upgrade &&
-    apt-get -y install build-essential openssl libreadline6 libreadline6-dev curl \
-      git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 \
-      libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool \
-      bison subversion
+######################################################################
+# Remove Vagrant ruby
+######################################################################
+remove_vagrant_ruby()
+{
+  # Vagrant hack
+  if [ -d /opt/ruby ]
+  then
+    rm -rf /opt/ruby
+  fi
+}
 
-    if [ -z "$(grep sshlogin /etc/group)" ]
-    then
-      groupadd sshlogin
-    fi
+######################################################################
+# Update base OS
+######################################################################
+update_base_os()
+{
+  # Upgrade headlessly (this is only safe-ish on vanilla systems)
+  # aptitude update &&
+  # apt-get -o dpkg::options::="--force-confnew" \
+  #     --force-yes -fuy dist-upgrade &&
 
-    if [ -z "$(getent passwd serveradmin)" ]
-    then
-      adduser serveradmin --gecos ",,," --disabled-password
-      adduser serveradmin sudo
-      adduser serveradmin sshlogin
-      echo serveradmin:$serveradminpassword | chpasswd
-    fi
+  apt-get update
+}
 
-    # echo "AllowGroups sshlogin" >> /etc/ssh/sshd_config
-    service ssh restart
+######################################################################
+# Install ruby
+######################################################################
+install_ruby()
+{
+  local rubyversion=$1
+  local rubydir=$2
+  local rubybinary=$3
+  if [ ! -f /usr/local/bin/ruby-build ]
+  then
+    mkdir /root/ruby-build
+    wget -q -O- "https://github.com/sstephenson/ruby-build/tarball/master" | tar -z -x -C/root/ruby-build --strip-components 1
+    cd /root/ruby-build
+    /root/ruby-build/install.sh
+    cd /root
+    rm -r /root/ruby-build
+  fi
 
-    # Install Ruby and Chef
-    # aptitude install -y ruby1.8 ruby1.8-dev make &&
-    # gem install --no-rdoc --no-ri chef --version 0.10.8
+  if [ ! -f $rubybinary ]
+  then
+    # Install development tools:
+    apt-get -y install build-essential curl
 
-    # sudo -u serveradmin -H bash serveradmin-install.sh
-fi
+    # Extras for RubyGems and Rails:
+    apt-get -y install zlib1g-dev libssl-dev
 
-# if ! test -f "/usr/local/rvm"
-# then
-#   bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)
-# fi
+    # Readline Dev on Ubuntu 10.04 LTS:
+    apt-get -y install libreadline5-dev
 
-# "$chef_binary" -c solo.rb -j solo.json
+    echo /usr/local/bin/ruby-build $rubyversion $rubydir
+    /usr/local/bin/ruby-build $rubyversion $rubydir
+  fi
+}
+
+######################################################################
+# Install chef
+######################################################################
+install_chef()
+{
+  local gembinary=$1
+  if [ -z "$(which chef-solo &> /dev/null)" ]
+  then
+    $gembinary install --no-rdoc --no-ri chef
+  fi
+}
+
+######################################################################
+# Configure chef
+######################################################################
+configure_chef()
+{
+  mkdir -p /etc/chef
+  echo "file_cache_path \"/tmp/chef-solo\"" > /etc/chef/solo.rb
+  echo "cookbook_path \"/tmp/chef-solo/cookbooks\"" >> /etc/chef/solo.rb
+
+  echo '{
+  "chef_server": {
+    "server_url": "http://localhost:4000",
+    "webui_enabled": true,
+    "init_style": "runit"
+  },
+  "run_list": [ "recipe[chef-server::rubygems-install]" ]
+}' > ~/chef.json
+}
+
+######################################################################
+# Run chef
+######################################################################
+run_chef()
+{
+  local chefbinary=$1
+  $chefbinary -c /etc/chef/solo.rb -j ~/chef.json -r http://s3.amazonaws.com/chef-solo/bootstrap-latest.tar.gz
+}
+
+######################################################################
+# Create chef admin user
+######################################################################
+create_admin_chef_user()
+{
+  local knifebinary=$1
+  $knifebinary configure -i --defaults -u admin -r ''
+}
+
+export DEBIAN_FRONTEND=noninteractive
+remove_vagrant_ruby
+update_base_os
+install_ruby $rubyversion $rubydir $rubybinary
+install_chef $gembinary
+configure_chef
+run_chef $chefbinary
+create_admin_chef_user $knifebinary
+
 echo "Done"
