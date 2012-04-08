6fb3a2554ff12beee314473c1855da0870fb55f5
Jacob Atzen <jacob@incremental.dk>
2012-04-08 14:46:06 +0200

    Add script for setting up local knife client

---
+++ add-knife-client.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+#
+# This script can setup a knife client on a local machine if you have ssh
+# access to the chef server.
+#
+
+host=$1
+user=$2
+newuser=$3
+
+chefdir=`pwd`/.chef
+
+if [[ ! -n $1 || ! -n $2 || ! -n $3 ]]
+then
+  echo "Usage: ./add-knife-client.sh [host] [user] [newuser]"
+  exit 1
+fi
+
+mkdir .chef
+
+ssh $user@$host "sudo -H EDITOR=cat knife client create $newuser -n -a -f /tmp/$newuser.pem"
+scp $user@$host:/tmp/$newuser.pem .chef
+ssh $user@$host "sudo rm /tmp/$newuser.pem"
+
+ssh -q $user@$host 'sudo cat /etc/chef/validation.pem' > .chef/validation.pem
+
+cat > .chef/knife.rb <<EOT
+log_level                :info
+log_location             STDOUT
+node_name                '$newuser'
+client_key               '$chefdir/$newuser.pem'
+validation_client_name   'chef-validator'
+validation_key           '$chefdir/validation.pem'
+chef_server_url          'http://$host:4000'
+cache_type               'BasicFile'
+cache_options( :path => '$chefdir/.chef/checksums' )
+EOT
